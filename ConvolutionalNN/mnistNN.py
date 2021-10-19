65db84fdc0618ade1af19ad71f07cb77642970c3
rjridle <ryanridley46@gmail.com>
2021-10-19 12:54:07 -0500

    Convolutional NN now works using tensorflow.keras

--- ConvolutionalNN/mnistNN.py
+++ ConvolutionalNN/mnistNN.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 import numpy as np
@@ -10,29 +9,36 @@ import pickle
 
 
 CWD = os.getcwd()
-TRAINING_DIR = CWD + "/data/dogs-vs-cats/train"
-TESTING_DIR = CWD + "/data/dogs-vs-cats/test1"
-PICKLE_DIR = CWD + "/pickle"
-MODELS_DIR = CWD + "/models"
-IMG_SIZE = 200
+TRAINING_DIR = CWD + "/dogs-vs-cats/train"
+TESTING_DIR = CWD + "/dogs-vs-cats/test1"
+IMG_SIZE = 100
+mnist = tf.keras.datasets.mnist
+
 
 def main():
 
-    X_train, y_train = CreateTrainingData()
-    DC_model = CreateModel(X_train, y_train)
-    DC_model.save(MODELS_DIR + "/DC_IMSZ200_BSZ64_EP5")
+    X, y = mnsit.load_data()
 
-    #DC_model = keras.models.load_model(MODELS_DIR + "/DogsVsCats.model")
-    X_test = CreateTestingData()
-    predictions = DC_model.predict(X_test)
-    print("X.shape = ", X_test.shape)
-    print("predictions.shape = ", predictions.shape)
-    print("predictions =\n", predictions)
+    #model = Sequential()
+    #model.add(Conv2D(64, (3,3), input_shape= X.shape[1:]))
+    #model.add(Activation("relu"))
+    #model.add(MaxPooling2D(pool_size=(2,2)))
 
-    plt.imshow(X_test[0], cmap = "gray")
-    plt.show()
-    print("Prediction = ", predictions[0])
+    #model.add(Conv2D(64, (3,3)))
+    #model.add(Activation("relu"))
+    #model.add(MaxPooling2D(pool_size=(2,2)))
 
+    #model.add(Flatten())
+    #model.add(Dense(64))
+
+    #model.add(Dense(1))
+    #model.add(Activation('sigmoid'))
+
+    #model.compile(loss="binary_crossentropy",
+    #              optimizer="adam",
+    #              metrics=['accuracy'])
+
+    #model.fit(X, y, batch_size=32, epochs=10, validation_split=0.1)
 
 def CreateTrainingData():
     training_data = []
@@ -64,20 +70,30 @@ def CreateTestingData():
 
     for img in os.listdir(TESTING_DIR):
         try:
+            category = img.split('.')[0]
             img_array = cv2.imread(os.path.join(TESTING_DIR, img), cv2.IMREAD_GRAYSCALE)
             resized_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-            testing_data.append([resized_array])
+            testing_data.append([resized_array, int(category == "cat")])
         except Exception as e:
             print("Broken image. Passing")
             pass
 
-    X = np.array(testing_data).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-    X = X/255.0
-    return X
+    for features, label in training_data:
+        X.append(features)
+        y.append(label)
 
-def PickleData(X):
+    X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+    X = X/255.0
+    y = np.array(y)
+    return X, y
+
+def PickleData(X, y):
     pickle_out = open("X.pickle", "wb")
     pickle.dump(X, pickle_out)
+    pickle_out.close()
+
+    pickle_out = open("y.pickle", "wb")
+    pickle.dump(y, pickle_out)
     pickle_out.close()
 
 def CreateModel(X, y):
@@ -101,7 +117,8 @@ def CreateModel(X, y):
                   optimizer="adam",
                   metrics=['accuracy'])
 
-    model.fit(X, y, batch_size=64, epochs=5, validation_split=0.1)
+    model.fit(X, y, batch_size=128, epochs = 10, validation_split=0.1)
+
     return model
 
 
